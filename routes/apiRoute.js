7642787e0bdcfd0297f13826dc1ddc45397e01d8
carlos ordonez <ordonezdose@gmail.com>
2020-11-04 12:51:07 -0500

    updating homework functionality

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,33 +1,34 @@
-// const router = require("express").Router();
-// const store = require("../db/store");
-// const { get } = require("./htmlRoutes");
-// router.get("/api/notes", function (req, res) {
-//     store
-//     // this code will execute when the server receives a GET request to api/notes
-// getNotes();
-//     // read what is in db.json => using fs module
-//     fs.readFile("../db.store", function(err, data) { 
-//         // Check for errors 
-//         if (err) throw err; 
-//     })
-//     // send response with data
-//     router.post("/api/notes", function (req, res) {
-//         const newNote = req.body;
-//             // take new note and add it to db.json using fs module
-//             app.get("/api/notes", function(req, res){
-//                 res.json(data);
-//             })
-// //     // send response that the note was saved
-// app.post("/api/notes",function(req , res){
-// res.json(data);
-// })
-// });
-// // DELETE "/api/notes" deletes the note with an id equal to req.params.id
-// router.delete("/notes/:id", (req, res) => {
-//     store
-//         .removeNote(req.params.id)
-//         .then(() => res.json({ ok: true }))
-//         .catch((err) => res.status(500).json(err));
-//     });
-// })
-//     module.exports = router;
+const store = require("../server");
+
+router.get("/notes", function (req, res) {
+    store
+    res.json(notes);
+});
+
+fs.writeFile('/db/store', data, (err) => {
+    if (err) throw err;
+    console.log('Note taken');
+
+    app.post("/notes", function (req, res) {
+        store
+            .addNote(req.body)
+        res.json(notes);
+        if (err) throw err;
+    })
+
+    app.get("/api/notes", function (req, res) {
+        res.json(data);
+        res.json(true)
+    })
+
+    app.delete("/notes/:id", (req, res) => {
+        store
+            .removeNote(req.params.id)
+            .then(() => res.json({ ok: true }))
+            .catch((err) => res.status(500).json(err));
+    });
+})
+
+module.exports = router;
+
+  
